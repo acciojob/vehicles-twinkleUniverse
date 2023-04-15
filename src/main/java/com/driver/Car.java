3105664ec9cf3face95f8d32625e67d7feedf967
Twinkle <twinklenarnaware32@gmail.com>
2023-04-15 20:05:38 +0530

    Contest Solution

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -7,19 +7,29 @@ public class Car extends Vehicle {
     private int gears;
     private boolean isManual;
     private int currentGear;
+
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name);
         //Hint: Car extends Vehicle
+       // this.name=name;
+        this.currentGear=1;
+        this.wheels=wheels;
+        this.doors=doors;
+        this.gears=gears;
+        this.isManual=isManual;
+        this.type=type;
+        this.seats=seats;
     }
 
     public void changeGear(int newGear){
-
+        currentGear=newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
